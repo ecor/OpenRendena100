635275c2c6e2a1bab117496cb93bcd70801e1834
ecor <emanuele.cordano@gmail.com>
2017-04-06 23:56:39 +0200

    update... Rendenagit add . !

--- mapcover/test1.R
+++ mapcover/test1.R
@@ -40,7 +40,7 @@ names(year) <- names(mapfiles)
 names(year) <- str_replace_all(names(year),"-",".")
 
 
-years <- 2000:2009
+years <- 1996:2015
 mstack <- stack(mapfiles[year %in% years],alt_map)
 mdf <- as.data.frame(mstack)
 
@@ -50,9 +50,42 @@ names(mdf)[names(mdf)=="layer"] <- "altitude"
 
 mdfm <- melt(mdf,id="altitude")
 mdfm <- mdfm[which(mdfm$altitude>=500 & mdfm$altitude<2500),]
-mdfm$vale <- factor(mdfm$value)
-gg <- ggplot(data = mdfm, aes(x=altitude, y=value, by=variable, color=variable))
-gg <- gg+geom_ribbon()+facet_grid(variable ~ .)
+##mdfm$value <- factor(mdfm$value)
+index <- sprintf("%s_%02d",mdfm$variable,mdfm$value)
+altitude_min <- tapply(X=mdfm$altitude,INDEX=index,FUN=min,na.rm=TRUE)
+altitude_max <- tapply(X=mdfm$altitude,INDEX=index,FUN=max,na.rm=TRUE)
+altitude_q25 <- tapply(X=mdfm$altitude,INDEX=index,FUN=quantile,probs=0.25,na.rm=TRUE)
+altitude_median <- tapply(X=mdfm$altitude,INDEX=index,FUN=median,na.rm=TRUE)
+altitude_q75 <- tapply(X=mdfm$altitude,INDEX=index,FUN=quantile,probs=0.75,na.rm=TRUE)
+altitude_mean <- tapply(X=mdfm$altitude,INDEX=index,FUN=mean,na.rm=TRUE)
+
+nnn <- names(altitude_min)
+
+ss <- str_split(nnn,"_")
+
+dafm <- data.frame(
+		variable=sapply(X=ss,FUN=function(x){x[1]}),
+		nday=as.numeric(sapply(X=ss,FUN=function(x){x[2]})),
+		stringsAsFactors=FALSE
+		)
+
+		
+dafm$altitude_max <- altitude_max[nnn]
+dafm$altitude_q25 <- altitude_q25[nnn]
+dafm$altitude_median <- altitude_median[nnn]
+dafm$altitude_q75 <- altitude_q75[nnn]
+dafm$altitude_mean <- altitude_mean[nnn]
+dafm$year <- as.numeric(str_replace(dafm$variable,"Y",""))
+dafm$period <- "none"
+dafm$period[dafm$year>=1996 & dafm$year<=2005] <- "1996-2005"
+dafm$period[dafm$year>=2006 & dafm$year<=2015] <- "2006-2015"
+
+### 
+### FARE UN TAPPLTY PER AGGREGARE I GIORNI 
+#stop("")
+
+gg <- ggplot(data = dafm, aes(ymin=altitude_q25,ymax=altitude_q75, x=nday, by=period, color=period,fill=period))+geom_ribbon()
+##gg <- gg+facet_grid(variable ~ .) ##+geom_ribbon()
 	##	geom_point()+median_hilow(y=value,by=variable)
 
 #alt_map_df <- as.data.frame(alt_map,xy=TRUE)
